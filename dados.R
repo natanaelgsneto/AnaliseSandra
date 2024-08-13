1125fc7fb3ef1d6b8201b507c4a0020d0424f960
Natanael <ngsneto@gmail.com>
2024-08-12 23:08:57 -0300

    add

---
+++ dados.R
@@ -0,0 +1,11 @@
+library(readxl)
+BOMBEIRO_BAYEUX <- read_excel("C:/Users/natan/Downloads/BOMBEIRO_BAYEUX.xlsx")
+BOMBEIRO_BAYEUX
+View(BOMBEIRO_BAYEUX)
+options('max.print' = 100000)   # or whatever value you want
+getOption('max.print')
+
+head(BOMBEIRO_BAYEUX)
+saida = na.omit(BOMBEIRO_BAYEUX)
+saida
+semnas=replace(x = BOMBEIRO_BAYEUX, list = is.na(BOMBEIRO_BAYEUX), values = 0)
