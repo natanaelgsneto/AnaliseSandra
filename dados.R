018899e63a847caa804902896365093f54ab291f
Natanael <ngsneto@gmail.com>
2024-08-12 23:17:13 -0300

    add

--- dados.R
+++ dados.R
@@ -2,7 +2,7 @@ library(readxl)
 BOMBEIRO_BAYEUX <- read_excel("C:/Users/natan/Downloads/BOMBEIRO_BAYEUX.xlsx")
 BOMBEIRO_BAYEUX
 View(BOMBEIRO_BAYEUX)
-options('max.print' = 100000)   # or whatever value you want
+options('max.print' = 100000)   #or whatever value you want
 getOption('max.print')
 
 head(BOMBEIRO_BAYEUX)
