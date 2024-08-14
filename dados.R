e713880f4fb68021621b9ee4f6361394762b2f48
Natanael <natanaelguedessn@gmail.com>
2024-08-13 21:31:27 -0300

    Update dados.R

--- dados.R
+++ dados.R
@@ -13,21 +13,43 @@ getwd()
 bombeirosIdade = table(BOMBEIRO_BAYEUX$Idade)
 bombeirosIdade
 
+paste("A media de idade é  ", sep=": ", mean(BOMBEIRO_BAYEUX$Idade))
+
 bombeiroSexo = table(BOMBEIRO_BAYEUX$Sexo)
 bombeiroSexo
+
+paste("A media relacionada ao sexo  é  ", sep=": ", mean(BOMBEIRO_BAYEUX$Sexo))
+
 
 estadocivil = table(BOMBEIRO_BAYEUX$EstadoCivil)
 estadocivil
 
+paste("A media relacionada ao estado civil  é  ", sep=": ", mean(BOMBEIRO_BAYEUX$EstadoCivil))
+
+
 problemac = table(BOMBEIRO_BAYEUX$ProblemaC)
 problemac
+paste("A media relacionada ao ProblemaC  é  ", sep=": ", mean(BOMBEIRO_BAYEUX$ProblemaC))
 
 orientacao = table(BOMBEIRO_BAYEUX$Orientação)
 orientacao 
+paste("A media relacionada ao orientação  é  ", sep=": ", mean(BOMBEIRO_BAYEUX$Orientação))
+
 
 filhos = table(BOMBEIRO_BAYEUX$FIlhos)
 filhos
+paste("A media relacionada aos filhos   é  ", sep=": ", mean(BOMBEIRO_BAYEUX$FIlhos))
 
 religiao = table(BOMBEIRO_BAYEUX$Religião)
 religiao
+paste("A media relacionada ao religião  é  ", sep=": ", mean(BOMBEIRO_BAYEUX$Religião))
 
+medidareligiao = table(BOMBEIRO_BAYEUX$TipoReligião)
+medidareligiao
+paste("A media relacionada ao   é  TipoReligião", sep=": ", mean(BOMBEIRO_BAYEUX$TipoReligião))
+
+patente = table(BOMBEIRO_BAYEUX$Patente)
+paste("A media relacionada ao Patente  é  ", sep=": ", mean(BOMBEIRO_BAYEUX$Patente))
+
+
+patente
